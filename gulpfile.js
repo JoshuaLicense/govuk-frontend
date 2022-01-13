c7d871b56e318970b8da1d40bb581dc5a2180d99
Kevin Dew <kevindew@me.com>
2022-01-13 23:02:11 +0000

    WIP remove gulp-stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,6 @@ require('./tasks/gulp/watch.js')
 require('./tasks/gulp/copy-to-destination.js')
 require('./tasks/gulp/asset-version.js')
 require('./tasks/gulp/sassdoc.js')
-require('./tasks/gulp/stylelint.js')
 
 // Umbrella scripts tasks for preview ---
 // Runs js lint and compilation
@@ -24,10 +23,9 @@ gulp.task('scripts', gulp.series(
 ))
 
 // Umbrella styles tasks for preview ----
-// Runs scss lint and compilation
+// Runs scss compilation
 // --------------------------------------
 gulp.task('styles', gulp.series(
-  'scss:lint',
   'scss:compile'
 ))
 
@@ -38,14 +36,6 @@ gulp.task('copy:assets', () => {
   return gulp.src(paths.src + 'assets/**/*')
     .pipe(gulp.dest(taskArguments.destination + '/assets/'))
 })
-
-// All test combined --------------------
-// Runs js, scss and accessibility tests
-// --------------------------------------
-gulp.task('test', gulp.series(
-  'scss:lint',
-  'scss:compile'
-))
 
 // Copy assets task for local & heroku --
 // Copies files to
